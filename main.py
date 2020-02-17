d8695e6454c8f1e3f7fea8a5312668e745c828ad
Xu <thierryxu60@gmail.com>
2020-02-17 15:11:15 +0100

    code with functions

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
 from Tkinter import *
 from PIL import ImageTk, Image
 
-window = Tk()
+class App: 
 
-window.title("My first app")
-window.geometry("1080x720")
-window.minsize(480, 360)
-window.config(background = '#41B77F')
+    map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
 
-canvas = Canvas(window, width=555, height=820)
-canvas.pack()
-
-
-map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-
-height = 5
-length = 5
-
-def create_rectangle(length, height) : 
-    a = canvas.create_rectangle(0, 0, 25, 25, fill='white')
-    canvas.move(a, length, height)
-
-for i in range(0, len(map)):
-    for j in range(0, len(map[i])):
-        create_rectangle(length, height)
-        height = height + 25
-        #map[i][j] = j
     height = 5
-    length = length + 25
-    
-b = canvas.create_rectangle(0, 0, 25, 25, fill='black')
-canvas.move(b, 105, 105)
-c = canvas.create_rectangle(0, 0, 25, 25, fill='black')
-canvas.move(c, 130, 105)
-d = canvas.create_rectangle(0, 0, 25, 25, fill='black')
-canvas.move(d, 155, 105)
+    length = 5
 
-window.mainloop()
+    def __init__(self, master):
+        self.master = master
+        self.init()
+
+    def init(self):
+        self.master.title("My first app")
+        self.master.geometry("1080x720")
+        self.master.minsize(480, 360)
+        self.master.config(background = '#41B77F')
+
+        canvas = Canvas(self.master, width=555, height=820)
+        canvas.pack(expand=YES)
+
+        for i in range(0, len(self.map)):
+            for j in range(0, len(self.map[i])):
+                a = canvas.create_rectangle(0, 0, 25, 25, fill='white')
+                canvas.move(a, self.length, self.height)
+                self.height = self.height + 25
+                #map[i][j] = j
+            self.height = 5
+            self.length = self.length + 25
+        
+        #b = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+        #canvas.move(b, 105, 105)
+        #c = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+        #canvas.move(c, 130, 105)
+        #d = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+        #canvas.move(d, 155, 105)
+
+def main():
+    root = Tk()
+    App(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
