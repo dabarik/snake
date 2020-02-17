7df268f5ec23b8a953e18820148ab3103d5a90f5
Xu <thierryxu60@gmail.com>
2020-02-17 11:29:45 +0100

    creating interface

---
+++ main.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+
+window = Tk()
+
+window.title("My first app")
+window.geometry("1080x720")
+window.minsize(480, 360)
+window.config(background = '#41B77F')
+
+
+window.mainloop()
