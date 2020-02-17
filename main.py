fce05e9111c3cce1caee2f1de4521c3065aa0b67
Xu <thierryxu60@gmail.com>
2020-02-17 14:29:57 +0100

    interface-graphique

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from PIL import ImageTk, Image
 
 window = Tk()
 
@@ -7,5 +8,49 @@ window.geometry("1080x720")
 window.minsize(480, 360)
 window.config(background = '#41B77F')
 
+canvas = Canvas(window, width=555, height=820)
+canvas.pack()
+
+
+map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+height = 5
+length = 5
+
+def create_rectangle(length, height) : 
+    a = canvas.create_rectangle(0, 0, 25, 25, fill='white')
+    canvas.move(a, length, height)
+
+for i in range(0, len(map)):
+    for j in range(0, len(map[i])):
+        create_rectangle(length, height)
+        height = height + 25
+        #map[i][j] = j
+    height = 5
+    length = length + 25
+    
+b = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+canvas.move(b, 105, 105)
+c = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+canvas.move(c, 130, 105)
+d = canvas.create_rectangle(0, 0, 25, 25, fill='black')
+canvas.move(d, 155, 105)
 
 window.mainloop()
